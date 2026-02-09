9ce660594f9aba9b0cba81effba722b980763da8
Nathaivel <kotarokanaoko5@gmail.com>
2026-02-09 19:52:55 +0530

    home p[age

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import "./index.css";
-import App from "./pages/App.jsx";
+import { Home, NoteView } from "./pages/App.jsx";
 import NotFound from "./pages/Error.jsx";
 import Navbar from "./components/Navbar.jsx";
 import NoteEdit from "./pages/NoteEdit.jsx";
@@ -15,7 +15,8 @@ createRoot(document.getElementById("root")).render(
     <div className="w-full">
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<App />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/notes" element={<NoteView />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="*" element={<NotFound />} />
           <Route path="/note" element={<NoteEdit />} />
